39eff7b249fd03f1a7f7b9976996a0831e5a3701
CHAE <chaso1495@naver.com>
2024-12-04 13:46:10 +0900

    [fix] 일부 잘못된 표현 수정 (#19)

--- src/engine/src/rabbitescape/engine/behaviours/bridge/UpLeftBridgingStrategy.java
+++ src/engine/src/rabbitescape/engine/behaviours/bridge/UpLeftBridgingStrategy.java
@@ -2,7 +2,6 @@ package rabbitescape.engine.behaviours.bridge;
 
 import static rabbitescape.engine.Block.Shape.BRIDGE_UP_LEFT;
 import rabbitescape.engine.World;
-import rabbitescape.engine.behaviours.Bridging.BridgeType;
 import rabbitescape.engine.Rabbit;
 
 public class UpLeftBridgingStrategy extends BaseBridgingStrategy {
@@ -12,10 +11,5 @@ public class UpLeftBridgingStrategy extends BaseBridgingStrategy {
         rabbit.y--;
         world.changes.addBlock(createBridgeBlock(rabbit.x, rabbit.y, BRIDGE_UP_LEFT));
         return true;
-    }
-    
-    @Override
-    public BridgeType getBridgeType() {
-        return BridgeType.UP;
     }
 }
--- src/engine/src/rabbitescape/engine/behaviours/bridge/UpRightBridgingStrategy.java
+++ src/engine/src/rabbitescape/engine/behaviours/bridge/UpRightBridgingStrategy.java
@@ -2,7 +2,6 @@ package rabbitescape.engine.behaviours.bridge;
 
 import static rabbitescape.engine.Block.Shape.BRIDGE_UP_RIGHT;
 import rabbitescape.engine.World;
-import rabbitescape.engine.behaviours.Bridging.BridgeType;
 import rabbitescape.engine.Rabbit;
 
 public class UpRightBridgingStrategy extends BaseBridgingStrategy {
@@ -12,10 +11,5 @@ public class UpRightBridgingStrategy extends BaseBridgingStrategy {
         rabbit.y--;
         world.changes.addBlock(createBridgeBlock(rabbit.x, rabbit.y, BRIDGE_UP_RIGHT));
         return true;
-    }
-    
-    @Override
-    public BridgeType getBridgeType() {
-        return BridgeType.UP;
     }
 }

0cf6f1fa186b1c6902f7eb4ae937d082f1953747
CHAE <chaso1495@naver.com>
2024-12-05 13:54:54 +0900

    [refactor] 전략 패턴 관련 추상화 계층 수정 (#19)

---
+++ src/engine/src/rabbitescape/engine/behaviours/bridge/BridgeTypeProvider.java
@@ -0,0 +1,7 @@
+package rabbitescape.engine.behaviours.bridge;
+
+import rabbitescape.engine.behaviours.Bridging.BridgeType;
+
+public interface BridgeTypeProvider {
+    BridgeType getBridgeType();
+}

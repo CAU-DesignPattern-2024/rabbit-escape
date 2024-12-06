73ebd973820a5da45738228f482a26dc6849a96f
yymin1022 <yymin1022@gmail.com>
2024-12-06 12:25:03 +0900

    [feat] Fixed Imports for BaseToken Interface

--- src/engine/src/rabbitescape/engine/BaseToken.java
+++ src/engine/src/rabbitescape/engine/BaseToken.java
@@ -7,10 +7,10 @@ import rabbitescape.engine.Token.Type;
 public class BaseToken implements TokenComponent {
     protected int x;
     protected int y;
-    protected Token.Type type;
+    protected Type type;
     protected State state;
 
-    public BaseToken(int x, int y, Token.Type type) {
+    public BaseToken(int x, int y, Type type) {
         this.x = x;
         this.y = y;
         this.type = type;
@@ -20,6 +20,17 @@ public class BaseToken implements TokenComponent {
     @Override
     public void behave(World world, int x, int y) {
         // 기본 동작 구현
+        Block onBlock = world.getBlockAt(x, y);
+        Block belowBlock = world.getBlockAt(x, y + 1);
+        boolean still = (
+            BehaviourTools.s_isFlat(belowBlock)
+            || (onBlock != null)
+            || BridgeTools.someoneIsBridgingAt(world, x, y)
+        );
+
+        state = switchType(type, !still,
+            BehaviourTools.isSlope(belowBlock),
+            BehaviourTools.isSlope(onBlock));
     }
 
     @Override
@@ -28,7 +39,7 @@ public class BaseToken implements TokenComponent {
     }
 
     @Override
-    public Token.Type getType() {
+    public Type getType() {
         return type;
     }
 
@@ -52,24 +63,66 @@ public class BaseToken implements TokenComponent {
         this.y = y;
     }
 
-    protected State switchType(Token.Type type, boolean moving, boolean falling, boolean onSlope) {
+    protected State switchType(Type type, boolean moving, boolean slopeBelow, boolean onSlope) {
         switch (type) {
             case bash:
-                return moving ? TOKEN_BASH_FALLING : (falling ? TOKEN_BASH_FALLING : (onSlope ? TOKEN_BASH_ON_SLOPE : TOKEN_BASH));
+                return chooseState(moving, slopeBelow, onSlope,
+                    TOKEN_BASH_FALLING, TOKEN_BASH_STILL,
+                    TOKEN_BASH_FALL_TO_SLOPE, TOKEN_BASH_ON_SLOPE);
+
             case dig:
-                return moving ? TOKEN_DIG_FALLING : (falling ? TOKEN_DIG_FALLING : (onSlope ? TOKEN_DIG_ON_SLOPE : TOKEN_DIG));
+                return chooseState(moving, slopeBelow, onSlope,
+                    TOKEN_DIG_FALLING, TOKEN_DIG_STILL,
+                    TOKEN_DIG_FALL_TO_SLOPE, TOKEN_DIG_ON_SLOPE);
+
             case bridge:
-                return moving ? TOKEN_BRIDGE_FALLING : (falling ? TOKEN_BRIDGE_FALLING : (onSlope ? TOKEN_BRIDGE_ON_SLOPE : TOKEN_BRIDGE));
+                return chooseState(moving, slopeBelow, onSlope,
+                    TOKEN_BRIDGE_FALLING, TOKEN_BRIDGE_STILL,
+                    TOKEN_BRIDGE_FALL_TO_SLOPE, TOKEN_BRIDGE_ON_SLOPE);
+
             case block:
-                return moving ? TOKEN_BLOCK_FALLING : (falling ? TOKEN_BLOCK_FALLING : (onSlope ? TOKEN_BLOCK_ON_SLOPE : TOKEN_BLOCK));
+                return chooseState(moving, slopeBelow, onSlope,
+                    TOKEN_BLOCK_FALLING, TOKEN_BLOCK_STILL,
+                    TOKEN_BLOCK_FALL_TO_SLOPE, TOKEN_BLOCK_ON_SLOPE);
+
             case climb:
-                return moving ? TOKEN_CLIMB_FALLING : (falling ? TOKEN_CLIMB_FALLING : (onSlope ? TOKEN_CLIMB_ON_SLOPE : TOKEN_CLIMB));
+                return chooseState(moving, slopeBelow, onSlope,
+                    TOKEN_CLIMB_FALLING, TOKEN_CLIMB_STILL,
+                    TOKEN_CLIMB_FALL_TO_SLOPE, TOKEN_CLIMB_ON_SLOPE);
+
             case explode:
-                return moving ? TOKEN_EXPLODE_FALLING : (falling ? TOKEN_EXPLODE_FALLING : (onSlope ? TOKEN_EXPLODE_ON_SLOPE : TOKEN_EXPLODE));
+                return chooseState(moving, slopeBelow, onSlope,
+                    TOKEN_EXPLODE_FALLING, TOKEN_EXPLODE_STILL,
+                    TOKEN_EXPLODE_FALL_TO_SLOPE, TOKEN_EXPLODE_ON_SLOPE);
+
             case brolly:
-                return moving ? TOKEN_BROLLY_FALLING : (falling ? TOKEN_BROLLY_FALLING : (onSlope ? TOKEN_BROLLY_ON_SLOPE : TOKEN_BROLLY));
+                return chooseState(moving, slopeBelow, onSlope,
+                    TOKEN_BROLLY_FALLING, TOKEN_BROLLY_STILL,
+                    TOKEN_BROLLY_FALL_TO_SLOPE, TOKEN_BROLLY_ON_SLOPE);
+
             default:
                 throw new Token.UnknownType(type);
         }
+    }
+
+    private State chooseState(
+        boolean moving,
+        boolean slopeBelow,
+        boolean onSlope,
+        State falling,
+        State onFlat,
+        State fallingToSlope,
+        State onSlopeState
+    ) {
+        if (onSlope) {
+            return onSlopeState;
+        }
+        if (!moving) {
+            return onFlat;
+        }
+        if (slopeBelow) {
+            return fallingToSlope;
+        }
+        return falling;
     }
 }

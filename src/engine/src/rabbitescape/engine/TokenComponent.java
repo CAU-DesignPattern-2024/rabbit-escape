29d0e26db572acd4af75a59a09dc178b836a5a16
yymin1022 <yymin1022@gmail.com>
2024-12-08 03:54:10 +0900

    [feat] Applied Thing Inheritance for Token with Decorator Pattern

--- src/engine/src/rabbitescape/engine/TokenComponent.java
+++ src/engine/src/rabbitescape/engine/TokenComponent.java
@@ -6,7 +6,7 @@ import rabbitescape.engine.Token.Type;
 /**
  * TokenComponent 인터페이스는 모든 토큰의 기본 동작을 정의합니다.
  */
-public interface TokenComponent {
+public interface TokenComponent extends ShownOnOverlay {
     void behave(World world, int x, int y);
     State getState();
     Token.Type getType();
@@ -14,4 +14,6 @@ public interface TokenComponent {
     int getY();
     void setX(int x);
     void setY(int y);
+    // Thing 클래스의 필수 메서드 추가
+    void calcNewState(World world);
 }
--- src/engine/src/rabbitescape/engine/TokenDecorator.java
+++ src/engine/src/rabbitescape/engine/TokenDecorator.java
@@ -4,20 +4,31 @@ import rabbitescape.engine.ChangeDescription.State;
 import rabbitescape.engine.Token.Type;
 import rabbitescape.engine.TokenComponent;
 import rabbitescape.engine.World;
+import rabbitescape.engine.Thing;
 
 /**
  * TokenDecorator는 모든 데코레이터의 기본 클래스입니다.
  */
-public abstract class TokenDecorator implements TokenComponent {
+public abstract class TokenDecorator extends Thing implements TokenComponent {
     protected TokenComponent decoratedToken;
 
-    public TokenDecorator(TokenComponent token) {
+    public TokenDecorator(TokenComponent token, int x, int y, State state) {
+        super(x, y, state);
         this.decoratedToken = token;
     }
 
     @Override
     public void behave(World world, int x, int y) {
         decoratedToken.behave(world, x, y);
+    }
+
+    @Override
+    public void calcNewState(World world) {
+        // Thing의 추상 메서드 구현
+        // 데코레이터 패턴에 맞게 위임
+        if (decoratedToken instanceof Thing) {
+            ((Thing)decoratedToken).calcNewState(world);
+        }
     }
 
     @Override

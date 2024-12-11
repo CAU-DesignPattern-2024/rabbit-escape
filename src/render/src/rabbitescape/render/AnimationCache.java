51d5f7f03df4e5637b2c581b05828e3f351b8d66
chae <chaso1495@naver.com>
2024-12-11 14:54:09 +0900

    [fix] AnimationCache 지연로딩

--- src/render/src/rabbitescape/render/AnimationCache.java
+++ src/render/src/rabbitescape/render/AnimationCache.java
@@ -12,14 +12,6 @@ public class AnimationCache implements IAnimationCache
     {
         this.names = AnimationLoader.listAll();
         this.animations = new HashMap<>();
-
-        for ( String name : names )
-        {
-            if ( !name.equals( AnimationLoader.NONE ) )
-            {
-                this.animations.put( name, AnimationLoader.load( name ) );
-            }
-        }
     }
 
     @Override
@@ -29,8 +21,12 @@ public class AnimationCache implements IAnimationCache
     }
 
     @Override
-    public Animation get( String animationName )
-    {
-        return animations.get( animationName );
+    public Animation get(String animationName) {
+        // 애니메이션이 없으면 로딩 후 캐시에 추가
+        if (!animations.containsKey(animationName)) {
+            Animation animation = AnimationLoader.load(animationName);
+            animations.put(animationName, animation);
+        }
+        return animations.get(animationName);
     }
 }
